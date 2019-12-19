d35f2635901d31a1d7b3ceea2c0a96df1936665c
Elena <mementomori.dixi@gmail.com>
2019-12-19 22:38:39 +0300

    up to date

---
+++ Lesson3/Lesson3_task3.py
@@ -0,0 +1,18 @@
+""""
+В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+"""
+import random
+a = [random.randint(0,100) for _ in range(20)]
+print(a)
+
+index_min = 0
+index_max = 0
+
+for i in range(len(a)):
+    if a[i] > a[index_max]:
+        index_max = i
+    elif a[i] < a[index_min]:
+        index_min = i
+
+a[index_max], a[index_min] = a[index_min], a[index_max]
+print(f"{a}\nПоменяли местами элементы c индексами {index_min} и {index_max}")
